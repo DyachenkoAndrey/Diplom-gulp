14df31281fd49447a4829219f40e56aab34d9d54
Andrey <a.dyachenko@me.com>
2020-05-11 10:46:41 +0400

    123
    123

--- js/script.js
+++ js/script.js
@@ -35,3 +35,42 @@ $(document).keyup(function (e) {
     closeModal();
   }
 });
+
+// Slider
+
+$(".regular").slick({
+  infinite: true,
+  arrows: true,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  adaptiveHeight: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
